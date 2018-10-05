c61ca8db847a675f06de12701ba20e43157719bb
Giorgos Logiotatidis <seadog@sealabs.net>
2018-10-05 15:41:14 +0300

    Add logging.

--- relay.py
+++ relay.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 from telethon import TelegramClient, events
 
+import logging
+
 import config
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 client = TelegramClient(config.SESSION_NAME, config.API_ID, config.API_HASH)
 client.start()
@@ -19,10 +24,12 @@ async def my_event_handler(event):
     for chat_id, relays in RELAY_MAP.items():
         if chat_id == event.chat.id:
             for relay in relays:
+                logger.info(f'Sending message from {event.chat.id} to {relay}')
                 await client.send_message(relay, event.message)
             break
     else:
         for relay in RELAY_MAP.get('default', []):
+            logger.info(f'Sending message from {event.chat.id} default {relay}')
             await client.send_message(relay, event.message)
 
 client.run_until_disconnected()
