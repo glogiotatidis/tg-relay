8d64feb82fd9013071f34fe31921dcc037130133
Giorgos Logiotatidis <seadog@sealabs.net>
2018-10-06 00:01:56 +0300

    Make listchannels work with sync.

--- listchannels.py
+++ listchannels.py
@@ -1,15 +1,14 @@
+import telethon.sync
 from telethon import TelegramClient
+
 
 import config
 
 
-client = TelegramClient('session_name', config.API_ID, config.API_HASH)
+client = TelegramClient(config.SESSION_NAME, config.API_ID, config.API_HASH)
 client.start()
 
 
 print('Channel | ID')
 for dialog in client.get_dialogs():
     print(f'{dialog.name} | {dialog.entity.id}')
-
-
-client.send_message(269584728, 'foo')
